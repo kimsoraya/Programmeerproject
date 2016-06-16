af10321732474df76fac6c53b243b4e942807e98
kimsoraya <kim_schuiten@hotmail.com>
2016-06-16 14:34:25 +0200

    created popup menu and option to choose image from gallery

--- MyPersonalCookbook/app/src/main/java/com/example/kimschuiten/mypersonalcookbook30/TextRecipeActivity.java
+++ MyPersonalCookbook/app/src/main/java/com/example/kimschuiten/mypersonalcookbook30/TextRecipeActivity.java
@@ -2,8 +2,10 @@ package com.example.kimschuiten.mypersonalcookbook30;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Environment;
@@ -11,10 +13,13 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import java.io.File;
@@ -38,6 +43,7 @@ public class TextRecipeActivity extends AppCompatActivity {
     SQLiteDatabase sqLiteDatabase;
 
     static final int CAM_REQUEST = 1;
+    private static int RESULT_LOAD_IMG = 1;
     String mCurrentPhotoPath;
 
 
@@ -56,33 +62,85 @@ public class TextRecipeActivity extends AppCompatActivity {
     }
 
     /*
-   Add a photo of your dish
+   Open pop up menu to choose between photo or picture gallery
     */
     public void onAddPictureClick(View view) {
-        // Start camera intent
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        PopupMenu popupMenu = new PopupMenu(context, view);
+        popupMenu.inflate(R.menu.picture_menu);
+        popupMenu.show();
 
-        // Ensure that there's a camera activity to handle the intent
-        if(cameraIntent.resolveActivity(getPackageManager()) != null){
-            // Create the file where the photo should go
-            File photo = null;
-            try{
-                photo = createImageFile();
-            }catch (IOException ex){
-                // Error occurred while creating the File
+        // Set onclicklistener for the two optiosn
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                // Open camera when clicked on "Take Photo"
+                if (item.getItemId() == R.id.item1) {
+                    // Start camera intent
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    // Ensure that there's a camera activity to handle the intent
+                    if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                        // Create the file where the photo should go
+                        File photo = null;
+                        try {
+                            photo = createImageFile();
+                        } catch (IOException ex) {
+                            // Error occurred while creating the File
+                        }
+                        // Continue only if the File was successfully created
+                        if (photo != null) {
+                            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+                            startActivityForResult(cameraIntent, CAM_REQUEST);
+                        }
+                    }
+                }
+                // Go to gallery when clicked on "Gallery"
+                if (item.getItemId() == R.id.item2){
+                    // Create intent to open Gallery
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    // Start the Intent
+                    startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+                }
+                return false;
             }
-            // Continue only if the File was successfully created
-            if(photo != null){
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
-                startActivityForResult(cameraIntent, CAM_REQUEST);
-            }
-        }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Get the image and put it in the ImageView
-        showPhotoImageView.setImageURI(Uri.parse(mCurrentPhotoPath));
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                mCurrentPhotoPath = cursor.getString(columnIndex);
+                cursor.close();
+                // Set the Image in ImageView after decoding the String
+                showPhotoImageView.setImageBitmap(BitmapFactory
+                        .decodeFile(mCurrentPhotoPath));
+
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+        /*// Get the image and put it in the ImageView
+        showPhotoImageView.setImageURI(Uri.parse(mCurrentPhotoPath));*/
     }
 
 
